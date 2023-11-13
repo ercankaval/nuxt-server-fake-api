13251fa3fdd0c0acc7402258351fe6f8568a8ca6
Ercan Kaval <ercan@mii.com.tr>
2023-11-13 12:30:09 +0300

    first commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+const { data: products, pending } = await useAsyncData("products", () =>
+    $fetch("/api/products")
+);
+
+const refresh = () => refreshNuxtData("products");
+
+</script>
+
+<template>
+  <div>
+    <p>{{ pending ? "Loading..." : products }}</p>
+    <button @click="refresh">Refresh</button>
+  </div>
+</template>
